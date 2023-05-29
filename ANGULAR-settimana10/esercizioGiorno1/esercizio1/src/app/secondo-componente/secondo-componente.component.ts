654d0400df0293d1e233ac465f62fc2a810c8c49
lucawar <lucaguerra92@yahoo.it>
2023-05-29 19:05:14 +0200

    settimana10-esercizioGiorno1

---
+++ ANGULAR-settimana10/esercizioGiorno1/esercizio1/src/app/secondo-componente/secondo-componente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secondo-componente',
+  templateUrl: './secondo-componente.component.html',
+  styleUrls: ['./secondo-componente.component.scss']
+})
+export class SecondoComponenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ANGULAR-settimana10/esercizioGiorno1/esercizio1/src/app/terzo-componente/terzo-componente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terzo-componente',
+  templateUrl: './terzo-componente.component.html',
+  styleUrls: ['./terzo-componente.component.scss']
+})
+export class TerzoComponenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
