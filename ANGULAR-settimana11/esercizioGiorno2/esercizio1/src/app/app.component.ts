9765dd12e2b4eb4caaa4064490dbd2f656ceb9ef
lucawar <lucaguerra92@yahoo.it>
2023-06-06 16:59:37 +0200

    settimana11-esercizioGiorno2

---
+++ ANGULAR-settimana11/esercizioGiorno2/esercizio1/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component,ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'esercizio2';
+
+  @ViewChild ('contenitoreForm', {static: true}) contenitoreForm!: NgForm;
+
+  userForm = {
+    scelta: ''
+}
+
+  user: any = {
+    name: '',
+    alterEgo: '',
+    power: '',
+    enemy: '',
+    planet: '',
+    weakness: ''
+  }
+
+  ngOnInit(): void {
+    this.contenitoreForm.statusChanges?.subscribe(stato => {
+      console.log(this.contenitoreForm);
+      console.log(`Stato del form: ${stato}`);
+  });
+}
+
+submitForm() {
+  console.log(`Form inviato: ${this.contenitoreForm}`);
+  this.user.name = this.contenitoreForm.value.name;
+  this.user.alterEgo = this.contenitoreForm.value.alterEgo;
+  this.user.power = this.contenitoreForm.value.power;
+  this.user.enemy = this.contenitoreForm.value.enemy;
+  this.user.planet = this.contenitoreForm.value.planet;
+  this.user.weakness = this.contenitoreForm.value.weakness;
+
+  this.contenitoreForm.reset();
+}
+}
